0b81fd79d5d4c3c450339a2042a9f7ac489cbe2c
Jeff Lin <thejefflin@gmail.com>
2022-04-13 23:56:22 -0400

    commit tree cad737b8e556f02d19effd4e39385033cb2364f5

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution here
+
+function findMatching(drivers, string) {
+    const names = drivers.filter(driver => driver.toLowerCase() === string.toLowerCase());
+    return names;
+}
+
+function fuzzyMatch(drivers, string) {
+    const names = drivers.filter(driver => driver.startsWith(string));
+    return names;
+}
+
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+  function matchName(drivers, string) {
+      const names = drivers.filter(driver => driver.name === string);
+      return names;
+  }
